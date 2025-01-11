1663790a413319536b297bc0abc75e10150a40d2
Shubham <sp4363619@gmail.com>
2025-01-10 22:18:31 -0800

    Second Commit

--- src/Components/Pagenotfound.js
+++ src/Components/Pagenotfound.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../Style/style.css";
 
 function Pagenotfound() {
   return (
-    <div>Pagenotfound</div>
+    <div className="page-wrap d-flex flex-row align-items-center" style={{padding: "213px 0px"}}>
+      <div className="container">
+          <div className="row justify-content-center">
+              <div className="col-md-12 text-center">
+                  <span className="display-1 d-block">404</span>
+                  <div className="mb-4 lead">The page you are looking for was not found.</div>
+                  <Link to="" className="btn btn-link text-decoration-none">Back to Home</Link>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
